42bc25d698edc8d3d1bb2cf34881f187e84b24b9
mrjha4050 <mrjha4050@gmail.com>
2024-12-29 22:46:47 +0530

    new changes 2

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,14 @@ app.use(cors({
 }));
 
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://client-invoice-gen.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(express.json());
 
 mongoose
